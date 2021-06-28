a16d545e9ba2a236cc224969d70adff90546b11a
Raphael Ramalho <rsramalhovf@gmail.com>
2021-06-28 19:15:40 -0300

    update

---
+++ meuPrimeiroCodigo/meuPrimeiroCodigo.py
@@ -0,0 +1,4 @@
+print("Hello World")
+print(2 + 3)
+print('2+3')
+print('Oresultado de 2+3 é:', 2 + 3)
---
+++ meuPrimeiroCodigo/variaveisDadosETipos.py
@@ -0,0 +1,5 @@
+nota = 8.5
+disciplina = 'Logica de programação e Algoritimos'
+
+print(nota)
+print(disciplina)
