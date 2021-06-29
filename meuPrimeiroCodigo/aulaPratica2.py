f95025daf9d6156d9e789c4dffcfa2e73847a81d
Raphael Ramalho <rsramalhovf@gmail.com>
2021-06-28 21:05:11 -0300

    update

---
+++ meuPrimeiroCodigo/aulaPratica2.py
@@ -0,0 +1,20 @@
+Qa = 1 + 2 + 3 + 4 + 5
+print(Qa)
+
+a = 23
+b = 19
+c = 31
+Qb = (a + b + c)/3
+print(Qb)
+
+Qc = 403//73
+print(Qc)
+
+Qd = 403 % 73
+print(Qd)
+
+Qe = 2**10
+print(Qe)
+
+Qf = 
+
--- meuPrimeiroCodigo/variaveisDadosETipos.py
+++ meuPrimeiroCodigo/variaveisDadosETipos.py
@@ -1,5 +1,15 @@
 nota = 8.5
 disciplina = 'Logica de programação e Algoritimos'
 
-print(nota)
-print(disciplina)
+a = 5
+b = 6
+
+#print(nota)
+#print(disciplina)
+
+res = a == b
+
+#print(res)
+
+s1 = "você tirou %f na disciplina de algooritimos" % nota
+print(s1)
