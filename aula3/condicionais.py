04c4c2a64acff779936b38159b05ba6c63ec71f9
Raphael Ramalho <rsramalhovf@gmail.com>
2021-06-29 21:06:47 -0300

    Update condicionais.py

--- aula3/condicionais.py
+++ aula3/condicionais.py
@@ -1,5 +1,14 @@
-x = int(input("Digite um valor inteiro: "))
-y = int(input("Digite outro valor inteiro: "))
+# x = int(input("Digite um valor inteiro: "))
+# y = int(input("Digite outro valor inteiro: "))
 
-if( x > y):
-    print("{} é maior do que {}" .format(x, y))
+# if( x > y):
+#     print("{} é maior do que {}" .format(x, y))
+
+
+
+x = int(input("Digite um valor inteiro: "))
+
+if (x % 2 == 0):
+    print("{} é par" .format(x))
+else:
+    print("{} é impar".format(x))
