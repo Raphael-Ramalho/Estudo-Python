c0a87dbf5c79258868375fb6ad044f2deced1486
Raphael Ramalho <rsramalhovf@gmail.com>
2021-06-30 09:59:32 -0300

    Update aulaPratica3.py

--- aula3/aulaPratica3.py
+++ aula3/aulaPratica3.py
@@ -50,3 +50,13 @@
 #     print("VocÊ escolheu um caminho!")
 
 
+lado1 = int(input("Digite o valor do lado1: "))
+lado2 = int(input("Digite o valor do lado2: "))
+lado3 = int(input("Digite o valor do lado3: "))
+
+if(lado1 == lado2 and lado1 == lado3):
+    print("Triangulo equilátero")
+elif(lado1 == lado2 or lado1 == lado3 or lado2 == lado3):
+    print("Triangulo Isósceles")
+else:
+    print("Triangulo Escaleno")
