ecfbec3addc4f039e1a0a12fa67557ffa490d7ca
Raphael Ramalho <rsramalhovf@gmail.com>
2021-06-30 09:40:20 -0300

    Update aulaPratica3.py

--- aula3/aulaPratica3.py
+++ aula3/aulaPratica3.py
@@ -9,9 +9,26 @@
 # d = (2 + 4 + 6) > 12
 # print(d)
 
-print(1387 % 19 == 0)
+# print(1387 % 19 == 0)
 
-print(31%2 == 0)
+# print(31%2 == 0)
 
-print(min(34, 29, 31) < 30)
+# print(min(34, 29, 31) < 30)
 
+# idade = int(input("Digite sua idade: "))
+# if(idade > 60):
+#     print("Você tem direito aos benefícios")
+
+
+
+# dano = int(input("Digite o dano: "))
+# escudo = int(input("Digite o valor do escudo: "))
+# if(dano>10 and escudo == 0):
+#     print("Você está morto")
+
+norte = False
+sul = False
+leste = False
+oeste = True
+if (norte or sul or leste or oeste):
+    print("Você escapou")
