cb0f61e50b8000490725d00d007152618c3e7685
Raphael Ramalho <rsramalhovf@gmail.com>
2021-06-29 21:29:31 -0300

    Update condicionais.py

--- aula3/condicionais.py
+++ aula3/condicionais.py
@@ -6,9 +6,20 @@
 
 
 
-x = int(input("Digite um valor inteiro: "))
+# x = int(input("Digite um valor inteiro: "))
 
-if (x % 2 == 0):
-    print("{} é par" .format(x))
+# if (x % 2 == 0):
+#     print("{} é par" .format(x))
+# else:
+#     print("{} é impar".format(x))
+
+
+
+x = int(input("Digite a nota da matéria 1: "))
+y = int(input("Digite a nota da matéria 1: "))
+z = int(input("Digite a nota da matéria 1: "))
+
+if((x>7) and (y>7) and (z>7)):
+    print("O aluno passou de ano")
 else:
-    print("{} é impar".format(x))
+    print("O aluno foi reprovado")
