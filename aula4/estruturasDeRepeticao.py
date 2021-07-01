f9d8003d46e29e8b9dc20dbceab27035754fa13d
Raphael Ramalho <rsramalhovf@gmail.com>
2021-07-01 10:44:14 -0300

    Create estruturasDeRepeticao.py

---
+++ aula4/estruturasDeRepeticao.py
@@ -0,0 +1,29 @@
+from typing import final
+
+
+# a = int(input("Qual valor deseja iniciar a contagem? "))
+# b = int(input("Qual valor deseja finalizar a contagem?"))
+# while(a <= b):
+#     if(a % 2 == 0):
+#         print(a)
+#     a = a + 1
+
+
+# n1 = float(input("Nota 1: "))
+# n2 = float(input("Nota 2: "))
+# n3 = float(input("Nota 3: "))
+# n4 = float(input("Nota 4: "))
+# n5 = float(input("Nota 5: "))
+
+# media = (n1 + n2 + n3 + n4 + n5)/5
+
+x=[]
+i=0
+total = 0
+numNotas = 5
+while(i<=(numNotas-1)):
+    x.append(float(input("Nota {}: ".format(i+1))))
+    total = total + x[i]
+    i=i+1
+media = total/numNotas
+print(media)
