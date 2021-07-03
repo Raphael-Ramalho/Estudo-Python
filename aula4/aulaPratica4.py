f79284bae3f5b610e86af7617033b07943a30cab
Raphael Ramalho <rsramalhovf@gmail.com>
2021-07-03 16:18:09 -0300

    Update aulaPratica4.py

--- aula4/aulaPratica4.py
+++ aula4/aulaPratica4.py
@@ -8,6 +8,7 @@ print("Digite 's' para sair")
 operacao = 0
 while(operacao != "s"):
     operacao = input("Qual operação deseja fazer? ")
+    
     if(operacao == "+" or operacao == "-" or operacao == "*" or operacao == "/"):
         v1 = float(input("Digite o primeiro valor: "))
         v2 = float(input("Digite o segundo valor: "))
@@ -24,10 +25,16 @@ while(operacao != "s"):
         elif(operacao == "/"):
             v3 = v1 / v2
             print("{} / {} = {}".format(v1, v2, v3))
+
+    elif(operacao == "s"):
+        break
+
     else:
-        print("Operação incompatível. Tente novamente.")
+        print("Operação inválida. Tente novamente.")
         print("+ Adição")
         print("- Subitração")
         print("* Multiplicação")
         print("/ Divisão")
         print("Digite 's' para sair")
+
+print("Programa encerrado.")
