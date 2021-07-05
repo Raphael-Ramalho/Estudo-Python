7d4501aa30e381f750ecefff5e71e83907938bda
Raphael Ramalho <rsramalhovf@gmail.com>
2021-07-05 00:30:57 -0300

    Update funcoes.py

--- aula5/funcoes.py
+++ aula5/funcoes.py
@@ -1,12 +1,24 @@
-def cabecalho (titulo):
-    if titulo:
-        bordaSI(titulo)
-        print("|{}|".format(titulo))
-        bordaSI(titulo)
+# def cabecalho (titulo):
+#     if titulo:
+#         bordaSI(titulo)
+#         print("|{}|".format(titulo))
+#         bordaSI(titulo)
 
-def bordaSI(titulo):
-    numTracos = len(titulo)
-    # print("+", "-"*numTracos, "+")
-    print("+{}+".format("-"*numTracos))
+# def bordaSI(titulo):
+#     numTracos = len(titulo)
+#     # print("+", "-"*numTracos, "+")
+#     print("+{}+".format("-"*numTracos))
 
-cabecalho("Ramalho")
+# cabecalho("Ramalho")
+
+
+def imprimeComCondicao(num, fcond):
+    if(fcond(num)):
+        print(num)
+
+def par(x):
+    return x % 2 == 0 
+def impar(x):
+    return not par(x)
+
+imprimeComCondicao(4,par)
