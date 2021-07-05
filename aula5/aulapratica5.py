ab5728f0a9c4ee62a495735d601f0fc4c5d86258
Raphael Ramalho <rsramalhovf@gmail.com>
2021-07-05 12:17:50 -0300

    Update aulapratica5.py

--- aula5/aulapratica5.py
+++ aula5/aulapratica5.py
@@ -1,22 +1,22 @@
-num = int(input("Digite um número positivo: "))
 
-def fatorial(num = 0):
+def fatorial(num):
     """
     funcao para calcular fatorial
     : param num: valor inteiro opicional 
     : return: resposta do fatorial 
     """
-    res = 1
-    while (num >= 1):
-        res *= num
-        num -= 1
-    return res
+    fat = 1
+    if (num == 0):
+        return fat
+    for i in range (1, num+1, 1):
+        fat = fat*i
+    return fat
 
-def valPositivo(num):
-    if(num >= 0):
-        return fatorial(num)
-    else:
-        return "valor invalido"
+def valida_int(pergunta, min, max):
+    num = int(input(pergunta))
+    while (num < min) or (num > max):
+        num = int(input(pergunta))
+    return num
         
-
-print(valPositivo(num))
+x = valida_int("Digite um valor para calcular o fatorial: ", 0, 50)
+print(fatorial(x))
