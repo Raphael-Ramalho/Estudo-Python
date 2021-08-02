27454a1f64ff26ab77b3ea97fd74ba84adbdff3d
Raphael-Ramalho <rsramalhovf@gmail.com>
2021-08-02 20:58:30 -0300

    Update meuPrimeiroCodigo.py

--- meuPrimeiroCodigo/meuPrimeiroCodigo.py
+++ meuPrimeiroCodigo/meuPrimeiroCodigo.py
@@ -1,4 +1,7 @@
-print("Hello World")
-print(2 + 3)
-print('2+3')
-print('Oresultado de 2+3 é:', 2 + 3)
+# print("Hello World")
+# print(2 + 3)
+# print('2+3')
+# print('O resultado de 2+3 é:', 2 + 3)
+
+
+
